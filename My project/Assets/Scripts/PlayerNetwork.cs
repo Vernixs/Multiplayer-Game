ffd47f051e77f65f4daa2d885b9364e452b48787
Vernar <enecelis2021@elam.co.uk>
2022-12-14 10:44:32 +0000

    new video tut

--- My project/Assets/Scripts/PlayerNetwork.cs
+++ My project/Assets/Scripts/PlayerNetwork.cs
@@ -5,22 +5,27 @@ using Unity.Netcode;
 
 public class PlayerNetwork : NetworkBehaviour
 {
-    private Rigidbody _rb;
-    private Camera _cam;
-    private void Awake()
-    {
-        _rb = GetComponent<Rigidbody>();
-        _cam = Camera.main;
-    }
 
-    public override void OnNetworkSpawn()
-    {
-        if (!IsOwner) Destroy(this);
-    }
+    private readonly NetworkVariable<PlayerNetworkData> _netState = new(writePerm: NetworkVariableWritePermission.Owner);
+    private Vector3 _vel;
+    private float _rotVel;
+    [SerializeField] private float _cheapInterpolationTime = 0.1f;
+
+
+
 
     private void Update()
     {
-
+        if (IsOwner)
+        {
+            _netPos.Value = transform.position;
+            _netRot.Value = transform.rotation;
+        }
+        else
+        {
+            transform.position = _netPos.Value;
+            transform.rotation = _netRot.Value;
+        }
 
         Vector3 moveDir = new Vector3(0, 0, 0);
 
@@ -31,5 +36,36 @@ public class PlayerNetwork : NetworkBehaviour
 
         float moveSpeed = 3f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+
+
+
+    struct PlayerNetworkData : INetworkSerializable
+    {
+        private float _x, _z;
+        private float _yRot;
+
+        internal Vector3 Position 
+        {
+            get => new Vector3(_x, 0, _z);
+            set
+            {
+                _x = value.x;
+                _z = value.z;
+            }
+        }
+
+        internal Vector3 Rotation
+        {
+            get => new Vector3(0, _yRot, 0);
+            set => _yRot = value.y;
+        }
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _x);
+            serializer.SerializeValue(ref _z);
+            serializer.SerializeValue(ref _yRot);
+        }
     }
 }
